c6414082dba7079491153e622392e4e96b1bcdc2
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-11-06 01:49:02 +0000

    Add wordbox component

--- src/components/WordBox.tsx
+++ src/components/WordBox.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+type WordBoxProps = {
+  guessedLetters: string[]
+  wordToGuess: string
+  reveal?: boolean
+}
 
-function WordBox() {
+function WordBox({
+  guessedLetters = ["z"], //Test: Fix Later
+  wordToGuess = "scz", //Test: Fix Later
+  reveal = false,
+}: WordBoxProps) {
   return (
-    <div>WordBox</div>
+    <div style={WordBoxComponent} >
+      {wordToGuess.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{ visibility: guessedLetters.includes(letter) || reveal ? "visible" : "hidden",
+              color: !guessedLetters.includes(letter) && reveal ? "red" : "black", }} >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
   )
 }
+
+const WordBoxComponent = {
+  display: "flex",
+  gap: ".25em",
+  fontSize: "6rem",
+  fontWeight: "bold",
+  textTransform: "uppercase",
+  fontFamily: "monospace",
+} as const;
 
 export default WordBox
