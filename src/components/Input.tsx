6cdd0bef77b449fbe6af54874ab54e87ae296365
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-11-06 02:07:27 +0000

    Add Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import styles from "./Input.module.css";
 
-function Input() {
+const KEYS = ["b","a","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+type InputProps = {
+  disabled?: boolean
+  activeLetters: string[]
+  inactiveLetters: string[]
+  addGuessedLetter: (letter: string) => void
+}
+
+
+function Input({
+  activeLetters = ["a"],
+  inactiveLetters = ["b"],
+  addGuessedLetter = (name)=> console.log(name),
+  disabled = false,
+}: InputProps) {
   return (
-    <div>Input</div>
+    <div style={Keyboard} >
+      {KEYS.map(key => {
+        const isActive = activeLetters.includes(key)
+        const isInactive = inactiveLetters.includes(key)
+        return (
+          <button onClick={() => addGuessedLetter(key)}
+            className = {`${styles.btn} ${isActive ? styles.active : ""} ${ isInactive ? styles.inactive : "" }`}
+            disabled={isInactive || isActive || disabled}
+            key={key} >
+            {key}
+          </button>
+        )
+      })}
+    </div>
   )
 }
+
+const Keyboard = {
+  display: "grid",
+  gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+  gap: ".5rem", 
+  alignSelf: "stretch"} as const;
 
 export default Input
